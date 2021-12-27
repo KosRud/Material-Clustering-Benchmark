7ddc75219492f50b33b46f5dfeb0f5e5ff106a46
Kostiantyn <hidden@none.com>
2021-12-27 04:09:57 +0200

    interactive refinement mostly working, but MSE log never shows decrease

--- Assets/HighlightRemovalTest.cs
+++ Assets/HighlightRemovalTest.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class HighlightRemovalTest : MonoBehaviour {
-	private const int numClusters = 16;
+	private const int numClusters = 6;
+	private const bool doRandomSwap = true;
+	private const bool doRandomInitialAttribution = false;
+	private const float timeStep = 0.5f;
+
+	private float timeLastIteration = 0;
 
 	private struct Position {
 		public int x;
@@ -13,8 +18,9 @@ public class HighlightRemovalTest : MonoBehaviour {
 	private ComputeBuffer cbufClusterCenters;
 	private ComputeBuffer cbufRandomPositions;
 	private RenderTexture rtInput;
-	private System.Random random = new System.Random(1);
-	private Position[] randomPositions = new Position[numClusters];
+	private readonly System.Random random = new System.Random(1);
+	private readonly Position[] randomPositions = new Position[numClusters];
+	private readonly Vector3[] clusterCenters = new Vector3[numClusters * 2];
 
 	public ComputeShader csHighlightRemoval;
 	public Texture2D texInput;
@@ -48,90 +54,87 @@ public class HighlightRemovalTest : MonoBehaviour {
 		this.cbufClusterCenters = new ComputeBuffer(numClusters * 2, sizeof(float) * 3);
 		this.cbufRandomPositions = new ComputeBuffer(numClusters, sizeof(int) * 2);
 
-		var clusterCenters = new Vector3[numClusters * 2];
-		for (int i = 0; i < clusterCenters.Length; i++) {
+		for (int i = 0; i < this.clusterCenters.Length; i++) {
 			// "old" cluster centers with infinite MSE
 			// to make sure new ones will overwrite them when validated
-			clusterCenters[i] = new Vector3(0, 0, Mathf.Infinity);
+			var c = Color.HSVToRGB(
+				//this.random.Next(10000) / 10000.0f,
+				i / (float)(numClusters),
+				1,
+				1
+			);
+			c *= 1.0f / (c.r + c.g + c.b);
+			this.clusterCenters[i] = new Vector3(c.r, c.g, Mathf.Infinity);
 		}
+		this.cbufClusterCenters.SetData(this.clusterCenters);
 
-		this.rtInput = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat) {
-			useMipMap = true,
-			autoGenerateMips = false,
-		};
+		this.rtInput = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
 		Graphics.Blit(this.texInput, this.rtInput);
-		this.rtInput.GenerateMips();
 
-		// initial clusters
+		if (doRandomInitialAttribution) {
+			int kh_AttributeInitialClusters = this.csHighlightRemoval.FindKernel("AttributeInitialClusters");
+			this.csHighlightRemoval.SetTexture(kh_AttributeInitialClusters, "tex_input", this.rtInput);
+			this.csHighlightRemoval.SetTexture(kh_AttributeInitialClusters, "tex_arr_clusters_rw", this.rtArr);
+			this.csHighlightRemoval.SetBuffer(kh_AttributeInitialClusters, "cbuf_cluster_centers", this.cbufClusterCenters);
+			this.csHighlightRemoval.Dispatch(kh_AttributeInitialClusters, 1024 / 32, 1024 / 32, 1);
 
-		int kh_AttributeInitialClusters = this.csHighlightRemoval.FindKernel("AttributeInitialClusters");
-		this.csHighlightRemoval.SetTexture(kh_AttributeInitialClusters, "tex_input", this.rtInput);
-		this.csHighlightRemoval.SetTexture(kh_AttributeInitialClusters, "tex_arr_clusters_rw", this.rtArr);
-		this.csHighlightRemoval.SetBuffer(kh_AttributeInitialClusters, "cbuf_cluster_centers", this.cbufClusterCenters);
-		this.csHighlightRemoval.Dispatch(kh_AttributeInitialClusters, 1024 / 32, 1024 / 32, 1);
-
-		void AttributeClusters(bool final) {
-			int kh_AttributeClusters = this.csHighlightRemoval.FindKernel("AttributeClusters");
-			this.csHighlightRemoval.SetBool("final", final);  // replace with define
-			this.csHighlightRemoval.SetTexture(kh_AttributeClusters, "tex_input", this.rtInput);
-			this.csHighlightRemoval.SetTexture(kh_AttributeClusters, "tex_arr_clusters_rw", this.rtArr);
-			this.csHighlightRemoval.SetBuffer(kh_AttributeClusters, "cbuf_cluster_centers", this.cbufClusterCenters);
-			this.csHighlightRemoval.Dispatch(kh_AttributeClusters, 1024 / 32, 1024 / 32, 1);
-		}
-
-		void KMeans() {
 			this.rtArr.GenerateMips();
 
-			UpdateRandomPositions();
-			int kh_UpdateClusterCenters = this.csHighlightRemoval.FindKernel("UpdateClusterCenters");
-			this.csHighlightRemoval.SetTexture(kh_UpdateClusterCenters, "tex_arr_clusters_r", this.rtArr);
-			this.csHighlightRemoval.SetTexture(kh_UpdateClusterCenters, "tex_input", this.rtInput);
-			this.csHighlightRemoval.SetBuffer(kh_UpdateClusterCenters, "cbuf_cluster_centers", this.cbufClusterCenters);
-			this.csHighlightRemoval.SetBuffer(kh_UpdateClusterCenters, "cbuf_random_positions", this.cbufRandomPositions);
-			this.csHighlightRemoval.Dispatch(kh_UpdateClusterCenters, 1, 1, 1);
+			this.UpdateClusterCenters();
+		}
 
-			AttributeClusters(false);
+		this.KMeans();
+		this.KMeans();
+		this.ValidateCandidates(); // guaranteed to succeed - MSE initialized with infinity
+	}
 
-			this.cbufClusterCenters.GetData(clusterCenters);
-			for (int i = 0; i < numClusters; i++) {
-				float MSE = clusterCenters[i].z;
-				if (MSE != Mathf.Infinity) {
-					Debug.Log($"MSE: {(int)(MSE * 1000000),8}");
-					break;
-				} else {
-					if (i == numClusters) {
-						Debug.Log("something went horribly wrong...");
-					}
+	private void AttributeClusters(bool final = false) {
+		int kh_AttributeClusters = this.csHighlightRemoval.FindKernel("AttributeClusters");
+		this.csHighlightRemoval.SetBool("final", final);  // replace with define
+		this.csHighlightRemoval.SetTexture(kh_AttributeClusters, "tex_input", this.rtInput);
+		this.csHighlightRemoval.SetTexture(kh_AttributeClusters, "tex_arr_clusters_rw", this.rtArr);
+		this.csHighlightRemoval.SetBuffer(kh_AttributeClusters, "cbuf_cluster_centers", this.cbufClusterCenters);
+		this.csHighlightRemoval.Dispatch(kh_AttributeClusters, 1024 / 32, 1024 / 32, 1);
+	}
+
+	private void UpdateClusterCenters() {
+		this.UpdateRandomPositions();
+
+		int kh_UpdateClusterCenters = this.csHighlightRemoval.FindKernel("UpdateClusterCenters");
+		this.csHighlightRemoval.SetTexture(kh_UpdateClusterCenters, "tex_arr_clusters_r", this.rtArr);
+		this.csHighlightRemoval.SetTexture(kh_UpdateClusterCenters, "tex_input", this.rtInput);
+		this.csHighlightRemoval.SetBuffer(kh_UpdateClusterCenters, "cbuf_cluster_centers", this.cbufClusterCenters);
+		this.csHighlightRemoval.SetBuffer(kh_UpdateClusterCenters, "cbuf_random_positions", this.cbufRandomPositions);
+		this.csHighlightRemoval.Dispatch(kh_UpdateClusterCenters, 1, 1, 1);
+	}
+
+	private void KMeans() {
+		this.AttributeClusters();
+		this.rtArr.GenerateMips();
+
+		this.UpdateClusterCenters();
+	}
+
+	private void LogMSE() {
+		this.cbufClusterCenters.GetData(this.clusterCenters);
+
+		for (int i = 0; i < numClusters; i++) {
+			float MSE = this.clusterCenters[i].z;
+			if (MSE != Mathf.Infinity) {
+				Debug.Log($"MSE: {(int)(MSE * 1000000),8}");
+				break;
+			} else {
+				if (i == numClusters) {
+					Debug.Log("something went horribly wrong...");
 				}
 			}
 		}
+	}
 
-		void ValidateCandidates() {
-			int kh_ValidateCandidates = this.csHighlightRemoval.FindKernel("ValidateCandidates");
-			this.csHighlightRemoval.SetBuffer(kh_ValidateCandidates, "cbuf_cluster_centers", this.cbufClusterCenters);
-			this.csHighlightRemoval.Dispatch(kh_ValidateCandidates, 1, 1, 1);
-		}
-
-		KMeans();
-		KMeans();
-		ValidateCandidates(); // guaranteed to succeed - MSE initialized with infinity
-
-		for (int i = 0; i < 3; i++) {
-			UpdateRandomPositions();
-
-			int kh_RandomSwap = this.csHighlightRemoval.FindKernel("RandomSwap");
-			this.csHighlightRemoval.SetBuffer(kh_RandomSwap, "cbuf_cluster_centers", this.cbufClusterCenters);
-			this.csHighlightRemoval.SetBuffer(kh_RandomSwap, "cbuf_random_positions", this.cbufClusterCenters);
-			this.csHighlightRemoval.SetTexture(kh_RandomSwap, "tex_input", this.rtInput);
-			this.csHighlightRemoval.SetInt("randomClusterCenter", this.random.Next(numClusters));
-			this.csHighlightRemoval.Dispatch(kh_RandomSwap, 1, 1, 1);
-
-			KMeans();
-			KMeans();
-			ValidateCandidates();
-		}
-
-		AttributeClusters(true);
+	private void ValidateCandidates() {
+		int kh_ValidateCandidates = this.csHighlightRemoval.FindKernel("ValidateCandidates");
+		this.csHighlightRemoval.SetBuffer(kh_ValidateCandidates, "cbuf_cluster_centers", this.cbufClusterCenters);
+		this.csHighlightRemoval.Dispatch(kh_ValidateCandidates, 1, 1, 1);
 	}
 
 	// Update is called once per frame
@@ -140,6 +143,39 @@ public class HighlightRemovalTest : MonoBehaviour {
 	}
 
 	private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		void ClusteringIteration() {
+			this.UpdateRandomPositions();
+
+			if (doRandomSwap) {
+				this.KMeans();  // update MSE in case if input changed
+
+				int kh_RandomSwap = this.csHighlightRemoval.FindKernel("RandomSwap");
+				this.csHighlightRemoval.SetBuffer(kh_RandomSwap, "cbuf_cluster_centers", this.cbufClusterCenters);
+				this.csHighlightRemoval.SetBuffer(kh_RandomSwap, "cbuf_random_positions", this.cbufClusterCenters);
+				this.csHighlightRemoval.SetTexture(kh_RandomSwap, "tex_input", this.rtInput);
+				this.csHighlightRemoval.SetInt("randomClusterCenter", this.random.Next(numClusters));
+				this.csHighlightRemoval.Dispatch(kh_RandomSwap, 1, 1, 1);
+
+				this.KMeans();
+				this.KMeans();
+
+				this.ValidateCandidates();
+			} else {
+				this.KMeans();
+				this.KMeans();
+				this.KMeans();
+			}
+		}
+
+		Graphics.Blit(this.texInput, this.rtInput);
+
+		if (Time.time - this.timeLastIteration > timeStep) {
+			this.timeLastIteration = Time.time;
+			ClusteringIteration();
+			this.AttributeClusters(true);
+			this.LogMSE();
+		}
+
 		int kh_ShowResult = this.csHighlightRemoval.FindKernel("ShowResult");
 		this.csHighlightRemoval.SetTexture(kh_ShowResult, "tex_arr_clusters_r", this.rtArr);
 		this.csHighlightRemoval.SetTexture(kh_ShowResult, "tex_output", this.rtResult);
