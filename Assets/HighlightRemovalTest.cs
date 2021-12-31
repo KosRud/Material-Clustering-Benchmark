c03514774a261386bfd8ddb07fdbcef55c3ff009
Kostiantyn <hidden@none.com>
2021-12-31 14:38:59 +0200

    update

--- Assets/HighlightRemovalTest.cs
+++ Assets/HighlightRemovalTest.cs
@@ -6,17 +6,16 @@ public class HighlightRemovalTest : MonoBehaviour {
 
 	private const int numClusters = 6;
 	private const bool doRandomSwap = false;
-	private const bool doRandomizeEmptyClusters = true;
+	private const bool doRandomizeEmptyClusters = false;
 	private const bool doKHM = false;
-	private const bool doOffset = false;
+	private const bool doJitter = false;
 
 	private const float timeStep = 1f;
 
 	private float timeLastIteration = 0;
 	private long? previousFrame = null;
 	private bool showReference = false;
-	private readonly int[][] offsets = new int[][] { new int[] { 0, 0 }, new int[] { 2, 0 }, new int[] { 0, 2 }, new int[] { 2, 2 }, new int[] { 1, 0 }, new int[] { 3, 0 }, new int[] { 1, 2 }, new int[] { 3, 2 }, new int[] { 1, 1 }, new int[] { 3, 1 }, new int[] { 1, 3 }, new int[] { 3, 3 }, new int[] { 0, 1 }, new int[] { 2, 1 }, new int[] { 0, 3 }, new int[] { 2, 3 }, };
-
+	private readonly int[][] offsets = new int[][] { new int[] { 0, 0 }, new int[] { 4, 0 }, new int[] { 0, 4 }, new int[] { 4, 4 }, new int[] { 2, 0 }, new int[] { 6, 0 }, new int[] { 2, 4 }, new int[] { 6, 4 }, new int[] { 0, 2 }, new int[] { 4, 2 }, new int[] { 0, 6 }, new int[] { 4, 6 }, new int[] { 2, 2 }, new int[] { 6, 2 }, new int[] { 2, 6 }, new int[] { 6, 6 }, new int[] { 1, 0 }, new int[] { 5, 0 }, new int[] { 1, 4 }, new int[] { 5, 4 }, new int[] { 3, 0 }, new int[] { 7, 0 }, new int[] { 3, 4 }, new int[] { 7, 4 }, new int[] { 1, 2 }, new int[] { 5, 2 }, new int[] { 1, 6 }, new int[] { 5, 6 }, new int[] { 3, 2 }, new int[] { 7, 2 }, new int[] { 3, 6 }, new int[] { 7, 6 }, new int[] { 1, 1 }, new int[] { 5, 1 }, new int[] { 1, 5 }, new int[] { 5, 5 }, new int[] { 3, 1 }, new int[] { 7, 1 }, new int[] { 3, 5 }, new int[] { 7, 5 }, new int[] { 1, 3 }, new int[] { 5, 3 }, new int[] { 1, 7 }, new int[] { 5, 7 }, new int[] { 3, 3 }, new int[] { 7, 3 }, new int[] { 3, 7 }, new int[] { 7, 7 }, new int[] { 0, 1 }, new int[] { 4, 1 }, new int[] { 0, 5 }, new int[] { 4, 5 }, new int[] { 2, 1 }, new int[] { 6, 1 }, new int[] { 2, 5 }, new int[] { 6, 5 }, new int[] { 0, 3 }, new int[] { 4, 3 }, new int[] { 0, 7 }, new int[] { 4, 7 }, new int[] { 2, 3 }, new int[] { 6, 3 }, new int[] { 2, 7 }, new int[] { 6, 7 }, };
 
 	private struct Position {
 		public int x;
@@ -235,7 +234,6 @@ public class HighlightRemovalTest : MonoBehaviour {
 		this.csHighlightRemoval.SetTexture(this.kernelGenerateMSE, "tex_input", this.rtReference);
 		this.csHighlightRemoval.SetTexture(this.kernelGenerateMSE, "tex_mse_rw", this.rtMSE);
 		this.csHighlightRemoval.SetBuffer(this.kernelGenerateMSE, "cbuf_cluster_centers", this.cbufClusterCenters);
-		this.csHighlightRemoval.SetTexture(this.kernelGenerateMSE, "tex_arr_clusters_r", this.rtArr);
 		this.csHighlightRemoval.Dispatch(
 			this.kernelGenerateMSE,
 			referenceTextureSize / 16,
@@ -330,7 +328,7 @@ public class HighlightRemovalTest : MonoBehaviour {
 		this.csHighlightRemoval.SetInt("sub_sample_multiplier", referenceTextureSize / textureSize);
 		this.csHighlightRemoval.SetInts(
 			"sub_sample_offset",
-			doOffset ?
+			doJitter ?
 				this.offsets[
 					Time.frameCount % this.offsets.Length
 				] :
