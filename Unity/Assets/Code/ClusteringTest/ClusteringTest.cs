99e7aca49d6f7bd7648a509f71bf4de4fcb6c1c1
Kostiantyn <hidden@none.com>
2022-04-11 01:33:02 +0200

    set up empy cluster randomization measurements

--- Unity/Assets/Code/ClusteringTest/ClusteringTest.cs
+++ Unity/Assets/Code/ClusteringTest/ClusteringTest.cs
@@ -179,7 +179,7 @@ public class ClusteringTest : MonoBehaviour {
   private void Awake() {
     Debug.Assert(this.videos.Length != 0);
 
-    new WorkGenerator.StaggeredJitter(
+    new WorkGenerator.EmptyClusterRandomization(
       kernelSize: kernelSize,
       videos: this.videos,
       csHighlightRemoval: this.csHighlightRemoval
