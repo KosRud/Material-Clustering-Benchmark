ef77a56e49265cbebb21f675058b5ea7584e84e7
Kostiantyn <hidden@none.com>
2022-04-17 07:13:45 +0200

    report parsing

--- node-notebook/util/unpackUnityJson.ts
+++ node-notebook/util/unpackUnityJson.ts
@@ -1,4 +1,8 @@
-export default (obj) => {
+import assert from 'assert/strict';
+
+export default function unpackUnityJson(obj) {
+    assert(obj.references);
+
     const refs = Object.fromEntries(
         obj.references.RefIds.map((reference) => [
             reference.rid,
@@ -6,7 +10,27 @@ export default (obj) => {
         ])
     );
 
-    return 1;
+    function fillRefs(obj) {
+        let nothingChanged = true;
 
-    return obj.references;
-};
+        do {
+            nothingChanged = true;
+
+            for (const attr in obj) {
+                const val = obj[attr];
+                if (typeof val == 'object') {
+                    if (val.rid) {
+                        obj[attr] = refs[val.rid];
+                        nothingChanged = false;
+                    } else {
+                        fillRefs(val);
+                    }
+                }
+            }
+        } while (nothingChanged == false);
+
+        return obj;
+    }
+
+    return fillRefs(obj);
+}
