0b5e14ca019895ee7ba78d1faa204c7927c51d01
Kostiantyn <hidden@none.com>
2022-04-30 04:53:59 +0200

    wip

--- node-notebook/anova/clustering.ts
+++ node-notebook/anova/clustering.ts
@@ -1,27 +1,68 @@
-type attributeClusters = (
-    data: number[][],
+import cluster from 'cluster';
+
+type attributeSamples = (
+    samples: number[][],
     attribution: number[],
     centers: number[][]
 ) => void;
 
 type updateCenters = (
-    data: number[][],
+    samples: number[][],
     attribution: number[],
     centers: number[][]
 ) => void;
 
 interface ClusteringAlgorithm {
-    attributeClusters: attributeClusters;
+    attributeSamples: attributeSamples;
     updateCenters: updateCenters;
 }
 
+function getVariance(
+    samples: number[][],
+    attribution: number[],
+    centers: number[][]
+): number {
+    return (
+        Object.entries(samples)
+            // go over all samples
+            .map((entry) => {
+                const [index, sample] = entry;
+                return { index, sample, cluster: centers[attribution[index]] };
+            })
+            .map(
+                // squared distances to cluster center
+                (sampleInfo) =>
+                    // go over all coordinates
+                    [...sampleInfo.cluster.keys()]
+                        .map(
+                            // differences between cluster coordinate and sample coordinate
+                            (coordIndex) =>
+                                sampleInfo.cluster[coordIndex] -
+                                sampleInfo.sample[coordIndex]
+                        )
+                        .map(
+                            // squared coordinate differences
+                            (difference) => difference ** 2
+                        )
+                        .reduce(
+                            // sum of squared coordinate differences
+                            (a, b) => a + b
+                        )
+            )
+            .reduce(
+                // sum of squared distances to cluster center
+                (a, b) => a + b
+            ) / samples.length
+    );
+}
+
 const Kmeans: ClusteringAlgorithm = {
-    attributeClusters: (
-        data: number[][],
+    attributeSamples: (
+        samples: number[][],
         attribution: number[],
         centers: number[][]
     ) => {
-        for (const [sampleIndex, sample] of Object.entries(data)) {
+        for (const [sampleIndex, sample] of Object.entries(samples)) {
             attribution[sampleIndex] = Object.entries(centers)
                 // go over each cluster center
                 .map((entry) => {
@@ -29,13 +70,13 @@ const Kmeans: ClusteringAlgorithm = {
                     return {
                         centerIndex,
                         center,
-                        distanceToSample: -1,
+                        distanceToSampleSq: -1,
                     };
                 })
                 .map(
-                    // generate L2 distance to the current sample for each center
+                    // generate squared distances to the current sample for each center
                     (centerInfo) => {
-                        centerInfo.distanceToSample =
+                        centerInfo.distanceToSampleSq =
                             // go over each coordinate
                             [...centerInfo.center.keys()]
                                 .map(
@@ -60,8 +101,8 @@ const Kmeans: ClusteringAlgorithm = {
                     // find the center with smallest distance to sample
                     (centerInfoA, centerInfoB) => {
                         if (
-                            centerInfoA.distanceToSample <
-                            centerInfoB.distanceToSample
+                            centerInfoA.distanceToSampleSq <
+                            centerInfoB.distanceToSampleSq
                         ) {
                             return centerInfoA;
                         } else {
@@ -73,14 +114,14 @@ const Kmeans: ClusteringAlgorithm = {
     },
 
     updateCenters: (
-        data: number[][],
+        samples: number[][],
         attribution: number[],
         centers: number[][]
     ) => {
         [...centers.keys()]
             // go over all centers
             .map((centerIndex) => {
-                centers[centerIndex] = Object.entries(data)
+                const mySamples = Object.entries(samples)
                     // go over all samples
                     .map((entry) => {
                         const [sampleIndex, sample] = entry;
@@ -97,7 +138,13 @@ const Kmeans: ClusteringAlgorithm = {
                     .map(
                         // now we only need coords
                         (sampleInfo) => sampleInfo.sample
-                    )
+                    );
+
+                if (mySamples.length == 0) {
+                    mySamples.push([...centers[centerIndex]]);
+                }
+
+                centers[centerIndex] = mySamples
                     .reduce((sampleA, sampleB) =>
                         // per-coordinate sum of samples
                         [...sampleA.keys()].map(
@@ -108,10 +155,10 @@ const Kmeans: ClusteringAlgorithm = {
                     )
                     .map(
                         // divide by the number of samples
-                        (coordValue) => coordValue / data.length
+                        (coordValue) => coordValue / mySamples.length
                     );
             });
     },
 };
 
-export { Kmeans };
+export { Kmeans, getVariance };
