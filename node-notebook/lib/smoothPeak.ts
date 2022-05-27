52a967deef85f24632e71f19b740603197fcc711
MadCake <->
2022-05-27 10:51:26 +0200

    smoothPeak plot

---
+++ node-notebook/lib/smoothPeak.ts
@@ -0,0 +1,15 @@
+export default function smoothPeak(inArr) {
+	const arr = inArr.slice();
+	const numParts = 100;
+	const partSize = Math.round(arr.length / numParts);
+	console.log(partSize);
+	const partialPeaks = [];
+	while (1) {
+		const nextPart = arr.splice(0, partSize);
+		if (nextPart.length == 0) {
+			break;
+		}
+		partialPeaks.push(nextPart.reduce((a, b) => Math.max(a, b)));
+	}
+	return partialPeaks.reduce((a, b) => a + b) / partialPeaks.length;
+}
