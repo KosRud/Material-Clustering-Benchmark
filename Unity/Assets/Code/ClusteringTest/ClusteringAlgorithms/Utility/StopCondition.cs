d607e009e49b2679c0c00d0c80f77837d453067d
MadCake <->
2022-06-01 09:47:45 +0200

    stop condition = 0 failed swaps allowed

--- Unity/Assets/Code/ClusteringTest/ClusteringAlgorithms/Utility/StopCondition.cs
+++ Unity/Assets/Code/ClusteringTest/ClusteringAlgorithms/Utility/StopCondition.cs
@@ -3,7 +3,7 @@ namespace ClusteringAlgorithms
     public static class StopCondition
     {
         public const float varianceChangeThreshold = 1e-4f;
-        public const int maxFailedSwaps = 1;
+        public const int maxFailedSwaps = 0;
         public const int maxIterations = 20;
     }
 }
