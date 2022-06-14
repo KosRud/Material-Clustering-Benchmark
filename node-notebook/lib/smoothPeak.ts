5b9a8bb7b83edc9fba41c4c4aa0e09e143713a7f
MadCake <->
2022-06-14 06:22:22 +0200

    wip

--- node-notebook/lib/smoothPeak.ts
+++ node-notebook/lib/smoothPeak.ts
@@ -1,17 +1,30 @@
 import assert from "assert/strict";
 
-export default function smoothPeak(inArr) {
+export default function smoothPeak(inArr, numParts) {
+
+	const partialPeaks = partitionArray(inArr, numParts).map(
+		arr => arr.reduce((a, b) => Math.max(a, b))
+	);
+	
+	// return mean peak
+	return partialPeaks.reduce((a, b) => a + b) / partialPeaks.length;
+}
+
+export function partitionArray(inArr, numParts){
+	const parts = [];
+
 	const arr = inArr.slice();
-	const numParts = 100;
 	const partSize = Math.round(arr.length / numParts);
-	//assert.ok(partSize > 100);
-	const partialPeaks = [];
+	if(partSize < 100) {
+		console.log(`warning: partSize=${partSize}`);
+	}
 	while (1) {
 		const nextPart = arr.splice(0, partSize);
 		if (nextPart.length == 0) {
 			break;
 		}
-		partialPeaks.push(nextPart.reduce((a, b) => Math.max(a, b)));
+		parts.push(nextPart);
 	}
-	return partialPeaks.reduce((a, b) => a + b) / partialPeaks.length;
+
+	return parts;
 }
